e7d9768de619914d31b5e4c3215340626e233ff6
me6edi <exited17@gmail.com>
2020-01-19 12:09:44 +0600

    variable

---
+++ Mahmud Ahsan/A.Python Tutorial/01.Python Tutorial for Beginners - Full Python Programming Crash Course_2.py
@@ -0,0 +1,73 @@
+#What is variable
+print("Hello world")
+
+name = "Mehedi Amin"
+age = 20
+country = "Bangladesh"
+
+print(name)
+print(age)
+print(country)
+
+
+name = "Grocery List"
+detail = 'Buy from supershop'
+number_of_items = 5
+budget = 2500# USD
+amount_of_rice = 1.56 #kg
+should_we_by_tody = True
+
+print(name, type(name))
+print(detail, type(detail))
+print(number_of_items, type(number_of_items))
+print(amount_of_rice, type(amount_of_rice))
+print(should_we_by_tody, type(should_we_by_tody))
+
+# Comments
+
+
+""""
+My name is Mehedi Amin
+"""
+print("Hello world")
+
+# Statements
+name = "Grocery List"
+detail = "Buy from supershop"
+number_of_items = 5
+number_of_ITEMS =10
+
+print(name,type(detail))
+print(detail,type(detail))
+print(number_of_items,type(number_of_items))
+
+#Expression
+a = 2
+b = a + 2
+
+# Operators
+number_of_items = 5
+number_of_items = number_of_items + 2
+print(number_of_items)
+
+number_of_items +=2
+print(number_of_items)
+
+amount_of_rice = 1.56
+
+amount_of_rice +=2.3
+print(amount_of_rice)
+
+number_of_items *=3
+print(number_of_items)
+number_of_items /=3
+print(number_of_items ,type(number_of_items))
+
+# Type casting
+
+number1 = 10
+number2 = 2
+result = number1 / number2
+print(number1,type(number1))
+print(number2,type(number2))
+print("Result is: ",result)
