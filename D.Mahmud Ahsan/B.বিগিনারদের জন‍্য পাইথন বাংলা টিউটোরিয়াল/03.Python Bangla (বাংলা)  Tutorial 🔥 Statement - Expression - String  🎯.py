c8f408d24a8e894d8f44f74af82bb32e8a9d4c16
me6edi <exited17@gmail.com>
2020-02-20 21:05:33 +0600

    String

---
+++ D.Mahmud Ahsan/B.বিগিনারদের জন‍্য পাইথন বাংলা টিউটোরিয়াল/03.Python Bangla (বাংলা)  Tutorial 🔥 Statement - Expression - String  🎯.py
@@ -0,0 +1,37 @@
+# Stacement
+print("Hello world")
+
+# Expression
+x = 10
+y = x + 2
+print(x,y)
+
+# String
+title = "Pyhon Course"
+print(title[0], title[1], title[2], title[-1], title[-2])
+
+# String Operation
+name = 'Mehedi Amin'
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(name.upper().lower().title())
+#String Concatenation
+first_name = "Steve"
+last_name = "Jobs"
+
+name = first_name +'\n'+ last_name
+print(name)
+
+name = "       Mr. X    "
+print('_' + name + '_')
+print('_' + name.lstrip()+ '_')
+
+# Printing Single and Double Quote
+shop_name = "Rahim's Shop"
+print(shop_name)
+shop_name = 'Rahim" Shop'
+print(shop_name)
+
+shop_name = 'Rahim\'s Shop'
+print(shop_name)
