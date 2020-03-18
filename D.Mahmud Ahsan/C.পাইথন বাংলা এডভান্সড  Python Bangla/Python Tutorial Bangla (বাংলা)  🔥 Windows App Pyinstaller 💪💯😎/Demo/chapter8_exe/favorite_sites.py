0547a6f2f5316fd2a1ffad2880d2aee0aec84391
Mehedi Amin <exited17@gmail.com>
2020-03-18 10:09:31 +0600

    Web API

---
+++ D.Mahmud Ahsan/C.পাইথন বাংলা এডভান্সড  Python Bangla/Python Tutorial Bangla (বাংলা)  🔥 Windows App Pyinstaller 💪💯😎/Demo/chapter8_exe/favorite_sites.py
@@ -0,0 +1,20 @@
+
+# --------------------------
+#      Executable in Windows
+# --------------------------
+
+import webbrowser
+
+def main():
+    print("Openning Favorite Sites")
+
+    with open("sites.txt") as fobj:
+        try:
+            for num, url in enumerate(fobj):
+                webbrowser.open_new_tab(url.strip())
+        except Exception as e:
+            print(e)
+
+    print("\nEnjoy!")
+
+if __name__ == '__main__' : main()
