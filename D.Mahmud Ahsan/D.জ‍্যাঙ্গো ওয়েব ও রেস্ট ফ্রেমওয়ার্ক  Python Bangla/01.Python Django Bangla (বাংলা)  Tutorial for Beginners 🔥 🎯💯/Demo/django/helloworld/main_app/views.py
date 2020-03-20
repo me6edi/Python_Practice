789deaf1c4f6122cbc2ad8cb7712edaa10602dac
Mehedi Amin <exited17@gmail.com>
2020-03-20 20:29:44 +0600

    Django-install&server-run

---
+++ D.Mahmud Ahsan/D.জ‍্যাঙ্গো ওয়েব ও রেস্ট ফ্রেমওয়ার্ক  Python Bangla/01.Python Django Bangla (বাংলা)  Tutorial for Beginners 🔥 🎯💯/Demo/django/helloworld/main_app/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def homeView(request):
+	return HttpResponse("<h1>Hello World</h1>")
--- D.Mahmud Ahsan/D.জ‍্যাঙ্গো ওয়েব ও রেস্ট ফ্রেমওয়ার্ক  Python Bangla/Python Bangla (বাংলা) ৩ Django REST API 👍 How to 🎯/ch4_model_admin/quotes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from . models import QuoteCategory
-from . models import Quote 
-
-admin.site.register(QuoteCategory)
-admin.site.register(Quote)
