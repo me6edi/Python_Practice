6b63f4499eb7a2bcb95dc87642a0a43b4c74f41f
Mehedi Amin <exited17@gmail.com>
2020-04-19 13:28:21 +0600

    CRUD Operations

---
+++ E.Crude Application/C.Complete Django CRUD Operations/Crud/employee_project/employee_register/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def employee_list(request):
+    return
+
+def employee_form(request):
+    return
+
+def employee_delete(request):
+    return
